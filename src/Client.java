ac688f170f90cfa419919deb95a013f43ed51cdd
tegao <onanefeosah@icloud.com>
2020-11-30 21:08:25 -0500

    script

--- src/Client.java
+++ src/Client.java
@@ -1,140 +1,236 @@
 import java.io.*;
 import java.net.Socket;
-import java.net.UnknownHostException;
-import java.util.Scanner;
+import java.util.*;
 
-public class Client extends Seeker{
+//Job Seeker
+public class Client {
 
-    static Creator c = new Creator();
+    private Socket s;
 
-    public static void main(String args[]) throws Exception {
-        Client c = new Client(new Job("null"));
+    private Scanner scan;
+    private String chat  = " ";
+    private String clientJob = " ";
+    private boolean hasJob;
+    private boolean doneJob;
+    private BufferedReader in;
+    private PrintStream out;
+    private String delimiter = ",";
+    private String choiceDelimiter = ";";
+    private int numChoiceFromServer;
+    private ArrayList<String> jobsReceivedByDelim = new ArrayList<>();
+
+    public static void main(String[] args){
+        new Client();
     }
 
-    public Client(Job j) {
-        super(j);
-        run();
-    }
-
-    public void run(){
-        try {
-            // Create client socket
-            Socket s = new Socket("localhost", 4999);
-            System.out.println("Connected");
-            String[] jobs = c.getCreator_jobs();
-            System.out.println("Available jobs: ");
-            for (int i = 0; i < jobs.length; i++) {
-                System.out.println(i + ". " + jobs[i]);
-            }
-
-            // to send data to the server
-            DataOutputStream dos = new DataOutputStream(s.getOutputStream());
-
-            // to read data from the keyboard
-            BufferedReader kb = new BufferedReader(new InputStreamReader(System.in));
-
-
-            // to read data coming from the server
-            BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
-
-            String str, str1;
-
-            // repeat as long as exit
-            // is not typed at client
-            while (!(str = kb.readLine()).equals("exit")) {
-
-                for (int i = 0; i < jobs.length; i++) {
-                    if (str.equals(jobs[i])) {
-                        str = "Client has picked " + jobs[i];
-                    }
-                }
-                // send to the server
-                dos.writeBytes(str + "\n");
-
-                // receive from the server
-                str1 = br.readLine();
-                if (str1.equals("Accept")) {
-                    setJ(new Job("Fishing"));
-                }else if(str1.equals("Decline")){
-                    //jobs.
-                }
-
-                System.out.println(str1);
-            }
-
-            // close connection.
-            dos.close();
-            br.close();
-            kb.close();
-            s.close();
-        }catch(Exception e){
-
-        }
-    }
-
-
-
-
-
-
-
-
-    /**
-    //Seeker seek  = new Seeker();
-    InputStreamReader reader;
-    BufferedReader read;
-    PrintStream stream;
-    Scanner scan;
-    Socket s;
-    String inputLine;
-    String outputLine;
-
-    public static void main(String[] args) throws Exception {
-        Client c = new Client();
-        c.run();
-    }
-
-    public void run() {
+    Client(){
         try{
-            try {
-                s = new Socket("localhost", 4999);
-                System.out.println("Connected to server!!");
-                reader = new InputStreamReader(s.getInputStream());
-                read = new BufferedReader(reader);
-                scan = new Scanner(s.getInputStream());
-                stream = new PrintStream(s.getOutputStream());
-                stream.println("New Client selecting from jobs");
-                stream.flush();
-                checkStream();
-
-
-                while ((inputLine = read.readLine()) != null) {
-                    //outputLine = kkp.processInput(inputLine);
-                    stream.println(inputLine);
-                    if (outputLine.equals("End"))
-                        s.close();
-                        break;
+            s = new Socket("localhost", 4999);
+            System.out.println("Connected to server " + s.getLocalAddress());
+            scan = new Scanner(System.in);
+            while(!s.isClosed()){
+                send(" ", 0);
+                receive();
+                menu();
+                System.out.print(">>>");
+                int choice  = scan.nextInt();
+                scan.nextLine();
+                while(choice<1 || choice> 5){
+                    System.out.print("Wrong input");
+                    System.out.print(">>>");
+                    choice = scan.nextInt();
+                    scan.nextLine();
                 }
+
+                switch(choice){
+                    case 1:
+                        if(hasJob){
+                            System.out.println("You currently have a job, Report your job status before selecting another");
+                        }
+                        else if(this.jobsReceivedByDelim.size()>1){
+                            send(" ", 1);
+                            receive();
+                            /**
+                            for(int i=0; i< this.jobsReceivedByDelim.size(); i++){
+                                System.out.println((i+1)+". " + this.jobsReceivedByDelim.get(i));
+                            }
+                             **/
+                            System.out.println("Job Available: " + this.jobsReceivedByDelim.get(0));
+                            System.out.println("1. Accept Job");
+                            System.out.println("2. Reject Job");
+                            int jobChoice = scan.nextInt();
+                            scan.nextLine();
+                            while(jobChoice < 1 || jobChoice > 2){
+                                System.out.println("Wrong choice");
+                                System.out.println("1. Accept Job");
+                                System.out.println("2. Reject Job");
+                                jobChoice = scan.nextInt();
+                                scan.nextLine();
+                            }
+                            if(jobChoice == 1){
+                                send("Accept", 21);
+                            }
+                            else if(jobChoice == 2){
+                                send("Reject", 22);
+                            }
+                            System.out.println("Job application submitted....");
+
+                        }
+
+                        else if(this.jobsReceivedByDelim.size()<1) {
+                            System.out.println("There are currently no Jobs available");
+                            System.out.println("Specify your skill and wait for the job creator");
+                        }
+
+
+                        break;
+                    case 2:
+                        System.out.println("Specify your service: ");
+                        String jobService = scan.nextLine();
+                        send(jobService, 2);
+                        System.out.println("Your skills have been logged, wait for a response...");
+                        break;
+
+                    case 3:
+                        if(this.hasJob) {
+                            System.out.println("Are you done with your Job");
+                            System.out.println("1. Yes");
+                            System.out.println("2. No");
+                            int thirdCaseChoice = scan.nextInt();
+                            scan.nextLine();
+                            while (thirdCaseChoice < 1 || thirdCaseChoice > 2) {
+                                System.out.println("Wrong input!!!");
+                                thirdCaseChoice = scan.nextInt();
+                                scan.nextLine();
+                            }
+                            if(thirdCaseChoice == 1){
+                                setDoneJob(true);
+                                setHasJob(false);
+                                send("Done," + getClientJob(), 3);
+                                setClientJob("No Job");
+                            }
+                            else{
+                                System.out.println("Ok..report back when you are done");
+                                continue;
+                            }
+                        }
+                        else{
+                            System.out.println("You do not have a job currently");
+                            continue;
+                        }
+                        break;
+                    case 4:
+                        if(getHasJob() && !getDoneJob()){
+                            System.out.println("You have not completed your job, are you sure you want to leave??");
+                            System.out.println("1. Yes");
+                            System.out.println("2. No");
+                            int fourthCaseChoice = scan.nextInt();
+                            while(fourthCaseChoice< 1 || fourthCaseChoice>2){
+                                System.out.println("Wrong input!!!");
+                                fourthCaseChoice = scan.nextInt();
+                            }
+                            if(fourthCaseChoice == 1){
+                                System.out.println("Thank you for connecting...BYE");
+                                s.close();
+                                break;
+                            }
+                            else if(fourthCaseChoice == 2){
+                                continue;
+                            }
+
+
+                        }
+                        else {
+                            System.out.println("Thank you for connecting...BYE");
+                            send(" ", 5);
+                            s.close();
+                            break;
+                        }
+                    break;
+                }
+
+                 //send(" ", 0);
+                 //receive();
             }
-            finally{
-                s.close();
+
+        }catch (IOException e){
+
+        }
+
+    }
+
+
+    public void send(String chat, int numChoice) throws IOException {
+        this.out = new PrintStream(s.getOutputStream());
+        chat = chat.concat(this.choiceDelimiter + numChoice);
+        this.out.println(chat);
+    }
+
+    public void receive() throws IOException {
+
+        this.in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        String receiveFromServer = this.in.readLine();
+
+        //if server response contains both delimiters
+        if(receiveFromServer.contains(this.choiceDelimiter)){
+            String[] broken = receiveFromServer.split(this.choiceDelimiter);
+            String brokenSentence = broken[0];
+            this.numChoiceFromServer = Integer.parseInt(broken[1]);
+
+            //response to client case 1 from server
+            if(this.numChoiceFromServer == 1){
+                this.jobsReceivedByDelim.clear();
+                String[] delimBrokenSentence = brokenSentence.split(this.delimiter);
+                this.jobsReceivedByDelim.addAll(Arrays.asList(delimBrokenSentence));
             }
-        }catch(Exception e){
-            System.out.println(e);
+
+
+            //response to client case 2,1 from server
+            if(this.numChoiceFromServer == 21){
+                System.out.println("You have been assigned the job: " + brokenSentence);
+                setHasJob(true);
+                setClientJob(brokenSentence);
+            }
+
+            if(this.numChoiceFromServer == 22){
+                System.out.println(brokenSentence);
+            }
+
         }
     }
 
-    public void checkStream(){
-        while(true){
-            receive();
-        }
+    public void menu(){
+         System.out.println("Welcome...this is the Job menu");
+         System.out.println("1. Available Jobs");
+         System.out.println("2. Specify Seeker Service");
+         System.out.println("3. Report Job Status");
+         System.out.println("4. Terminate Connection..");
+
     }
 
-    public void receive(){
-        if(scan.hasNext()){
-            String message = scan.nextLine();
-            System.out.println(message);
-        }
+
+    public String getClientJob() {
+        return clientJob;
     }
-     **/
+
+    public void setClientJob(String clientJob) {
+        this.clientJob = clientJob;
+    }
+
+    public void setHasJob(boolean hasJob) {
+        this.hasJob = hasJob;
+    }
+
+    public boolean getHasJob(){
+         return this.hasJob;
+    }
+
+    public void setDoneJob(boolean doneJob){
+         this.doneJob = doneJob;
+    }
+
+    public boolean getDoneJob(){
+         return this.doneJob;
+    }
+
 }
--- src/Server.java
+++ src/Server.java
@@ -4,56 +4,204 @@ import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Server extends Creator {
 
-    public static void main(String args[]) throws Exception {
+//Job creator
+public class Server {
 
-        // Create server Socket
-        ServerSocket ss = new ServerSocket(4999);
+
+    private ServerSocket ss;
+    private Socket s;
+    private BufferedReader in;
+    private PrintStream out;
+    private Scanner scan;
+    private String chat = " ";
+    private ArrayList<String> createdJobs = new ArrayList<>();
+    private String delimiter = ",";
+    private String choiceDelimiter = ";";
+    private ArrayList<String> clientJobSeekList = new ArrayList<>();
+    //private ArrayList<String> fff = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        new Server();
+    }
+
+    Server(){
+
+        try {
+        ss = new ServerSocket(4999);
         System.out.println("Waiting for Client....");
 
         // connect it to client socket
-        Socket s = ss.accept();
+        s = ss.accept();
         System.out.println("Connection established");
+        scan = new Scanner(System.in);
+        createJobs(); //TODO
 
-        // to send data to the client
-        PrintStream ps = new PrintStream(s.getOutputStream());
+        while(!s.isClosed()){
+            //menu();
+            receive();
+            send("", 0);
+            /**
+            System.out.print(">>>");
+            int choice  = scan.nextInt();
+            while(choice<1 || choice> 3){
+                System.out.print("Wrong input");
+                System.out.print(">>>");
+                choice = scan.nextInt();
+            }
+            switch(choice){
+                case 1:
 
-        // to read data coming from the client
-        BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+                    break;
+                case 2:
 
-        // to read data from the keyboard
-        BufferedReader kb = new BufferedReader(new InputStreamReader(System.in));
+                    break;
+                case 3:
+                    System.out.println("Thank you for connecting...BYE");
+                    s.close();
+                    break;
+            }
+        **/
 
-
-        // server executes continuously
-        while (true) {
-            String str, str1;
-
-            // repeat as long as the client
-            // does not send a null string
-
-            // read from client
-            while ((str = br.readLine()) != null) {
-                System.out.println(str);
-                str1 = kb.readLine();
+        }
 
 
-                // send to client
-                ps.println(str1);
+        }catch(IOException e) {
+            System.out.println(e);
+        }
+    }
+
+    private void createOneToOneJobs(){
+
+    }
+
+    private void createOneToManyJobs(){
+
+    }
+
+    private void createJobs() {
+        //this.createdJobs.add("One-To-One");
+        this.createdJobs.add("Fishing");
+        this.createdJobs.add("Teaching");
+        //this.createdJobs.add("One-To-Many");
+        this.createdJobs.add("Game Dev");
+    }
+
+    public void send(String chat, int numChoice) throws IOException {
+        this.out = new PrintStream(s.getOutputStream());
+        chat = chat.concat(this.choiceDelimiter + numChoice);
+        this.out.println(chat);
+    }
+
+    public void receive() throws IOException {
+        this.in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        String receiveFromServer = this.in.readLine();
+        if(receiveFromServer.contains(this.choiceDelimiter)){
+            String[] broken = receiveFromServer.split(this.choiceDelimiter);
+            String brokenSentence = broken[0];
+            int numChoiceFromServer = Integer.parseInt(broken[1]);
+
+            //client case 1
+            if(numChoiceFromServer == 1){
+                if(this.createdJobs.size()> 1) {
+                    StringBuilder sb = new StringBuilder();
+                    for (String j : this.createdJobs) {
+                        sb.append(j);
+                        sb.append(this.delimiter);
+                    }
+                    send(sb.toString(), 1);
+                }
+                else{
+
+                }
+            }
+            //client case 1,1
+            if(numChoiceFromServer == 11){
+                System.out.println("Job request from client");
+                System.out.println("1. Approve");
+                System.out.println("2. Decline");
+                System.out.println("3. Later");
+                int jobStatChoice = scan.nextInt();
+
+                if(jobStatChoice == 1) {
+                    send("Job Approved," + brokenSentence, 11);
+                    this.createdJobs.remove(brokenSentence);
+                }
+                else if(jobStatChoice == 2){
+                    send("Job Declined," + brokenSentence, 11);
+                }
+                else{
+                    send("Job request is in process", 11);
+                    this.clientJobSeekList.add(brokenSentence);
+                }
+            }
+            //client case 2
+            if(numChoiceFromServer == 2){
+                if(this.clientJobSeekList.size() < 1){
+                    System.out.println("The Client is looking for a job");
+                    System.out.println("Is there a job available???");
+                    System.out.println("1. Yes");
+                    System.out.println("2. No");
+                    int jobChoice = scan.nextInt();
+                    while(jobChoice<1 || jobChoice> 2){
+                        System.out.println("Wrong Input!!!");
+                        jobChoice = scan.nextInt();
+                    }
+                    scan.nextLine();
+                    if(jobChoice == 1){
+                        System.out.println("What job corresponds to the client: ");
+                        String job = scan.nextLine();
+                        this.clientJobSeekList.add(job);
+                        send(job, 21); //TODO
+                    }
+                    else if(jobChoice == 2){
+                        send("There are no jobs available currently. Check again later", 22);
+                    }
+                }
+                else{
+
+                }
+
+            }
+            //client case 2,1
+            if(numChoiceFromServer == 21){
+                this.clientJobSeekList.add(brokenSentence);
+            }
+            //client case 2,2
+            if(numChoiceFromServer == 22){
+                //TODO
+            }
+
+            //client case 3
+            if(numChoiceFromServer == 3){
+                String[] delimBrokenSentence = brokenSentence.split(this.delimiter);
+                if(delimBrokenSentence[0].equalsIgnoreCase("done")){
+                    this.createdJobs.remove(delimBrokenSentence[1]);
+                }
+            }
+
+            //client case 4
+            if(numChoiceFromServer == 4){
+                s.close();
             }
 
 
-            // close connection
-            ps.close();
-            br.close();
-            kb.close();
-            ss.close();
-            s.close();
+        }
+        else if(receiveFromServer.contains(this.delimiter)){
 
-            // terminate application
-            System.exit(0);
+        }
 
-        } // end of while
     }
+
+    public void menu(){
+        //System.out.println("Welcome...this is the Job menu");
+        System.out.println("1. Available Job Seekers");
+        System.out.println("2. Assign Jobs");
+        System.out.println("3. Terminate Connection..");
+    }
+
+
+
+
+
 }
